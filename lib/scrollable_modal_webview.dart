e2d9498dd1b5b9e792cd5f2ee866ebfed237c28c
Shuhei Yoshida <aneki525@gmail.com>
2022-12-28 11:53:49 +0900

    first commit

---
+++ lib/scrollable_modal_webview.dart
@@ -0,0 +1,17 @@
+library scrollable_modal_webview;
+
+import 'package:flutter/widgets.dart';
+
+class ScrollableModalWebView extends StatefulWidget {
+  const ScrollableModalWebView({super.key});
+
+  @override
+  State<ScrollableModalWebView> createState() => _ScrollableModalWebViewState();
+}
+
+class _ScrollableModalWebViewState extends State<ScrollableModalWebView> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
