d0fe3e6e87dc5535ed4e12c8f09434c3d46bd7ce
KobayashiYoh <y.kobayashi@4nonome.com>
2023-01-28 17:31:39 +0900

    add showScrollableModalWebView method

--- lib/scrollable_modal_webview.dart
+++ lib/scrollable_modal_webview.dart
@@ -1,6 +1,28 @@
 library scrollable_modal_webview;
 
-import 'package:flutter/widgets.dart';
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+void showScrollableModalWebView({
+  /// Shows a scrollable modal WebView.
+  required WebViewController controller,
+  required String url,
+  required BuildContext context,
+  PreferredSizeWidget? header,
+  bool scrollable = true,
+  double initialChildSize = 1.0,
+  double radius = 10.0,
+}) {
+  if (!(Platform.isIOS || Platform.isAndroid)) {
+    throw Exception('This OS is not supported');
+  }
+  showModalBottomSheet(
+    context: context,
+    builder: (BuildContext context) => const ScrollableModalWebView(),
+  );
+}
 
 class ScrollableModalWebView extends StatefulWidget {
   const ScrollableModalWebView({super.key});
