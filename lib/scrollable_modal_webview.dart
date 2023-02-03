b1854674ee471d8baf2c4176fb042da04ec921bc
Yoshida-koshi <koshi2278@outlook.jp>
2023-02-03 15:50:25 +0900

    add safeArea

--- lib/scrollable_modal_webview.dart
+++ lib/scrollable_modal_webview.dart
@@ -14,12 +14,15 @@ void showScrollableModalWebView({
   bool scrollable = true,
   double initialChildSize = 1.0,
   ShapeBorder? shape,
+  Color? backgroundColor,
 }) {
   if (!(Platform.isIOS || Platform.isAndroid)) {
     throw Exception('This OS is not supported');
   }
+  final double statusBarHeight = MediaQuery.of(context).padding.top;
   showModalBottomSheet(
     context: context,
+    backgroundColor: backgroundColor,
     isScrollControlled: true,
     shape: shape,
     builder: (BuildContext context) => ScrollableModalBottomSheet(
@@ -28,6 +31,7 @@ void showScrollableModalWebView({
       scrollable: scrollable,
       url: url,
       header: header,
+      statusBarHeight: statusBarHeight,
     ),
   );
 }
@@ -38,6 +42,7 @@ class ScrollableModalBottomSheet extends StatelessWidget {
   final double initialChildSize;
   final bool scrollable;
   final String url;
+  final double statusBarHeight;
   const ScrollableModalBottomSheet({
     Key? key,
     required this.controller,
@@ -45,21 +50,29 @@ class ScrollableModalBottomSheet extends StatelessWidget {
     required this.initialChildSize,
     required this.scrollable,
     required this.url,
+    required this.statusBarHeight,
   }) : super(key: key);
   @override
   Widget build(BuildContext context) {
+    final double deviceHeight = MediaQuery.of(context).size.height;
     return DraggableScrollableSheet(
         expand: false,
         initialChildSize: initialChildSize,
         builder: (context, scrollController) {
           return Column(
             children: [
+              if (initialChildSize >=
+                  1.0 * (deviceHeight - statusBarHeight) / deviceHeight)
+                SizedBox(height: statusBarHeight),
               if (header != null) header!,
               Expanded(
-                  child: SingleChildScrollView(
-                physics:
-                    scrollable ? null : const NeverScrollableScrollPhysics(),
-                child: const ScrollableModalWebView(),
+                  child: Container(
+                color: Colors.white,
+                child: SingleChildScrollView(
+                  physics:
+                      scrollable ? null : const NeverScrollableScrollPhysics(),
+                  child: const ScrollableModalWebView(),
+                ),
               ))
             ],
           );
