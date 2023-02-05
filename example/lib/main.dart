7ed277aa5741eb1e057267a0273b8d3c012e92a7
Yoshida-koshi <koshi2278@outlook.jp>
2023-02-05 15:16:41 +0900

    add three patterns about header and safeArea

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -39,28 +39,89 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: TextButton(
-          child: const Text("display showScrollableModalWebView"),
-          onPressed: () => showScrollableModalWebView(
-              context: context,
-              header: Container(
-                height: 30,
-                decoration: const BoxDecoration(
-                  color: Colors.blue,
+      body: Column(
+        children: [
+          Expanded(child: Container()),
+          TextButton(
+            child: const Text(
+                "display showScrollableModalWebView (has a header and a safeArea)"),
+            onPressed: () => showScrollableModalWebView(
+                context: context,
+                header: Container(
+                  height: 30,
+                  decoration: const BoxDecoration(
+                    color: Colors.blue,
+                  ),
+                  child: const Center(
+                    child: Text('ScrollableModalBottomSheet'),
+                  ),
                 ),
-                child: const Center(
-                  child: Text('ScrollableModalBottomSheet'),
+                backgroundColor: Colors.blue,
+                controller: controller,
+                initialChildSize: 0.96,
+                url: 'https://flutter.dev/',
+                shape: const RoundedRectangleBorder(
+                  borderRadius:
+                      BorderRadius.vertical(top: Radius.circular(10.0)),
+                )),
+          ),
+          Expanded(child: Container()),
+          TextButton(
+            child: const Text(
+                "display showScrollableModalWebView (has a header but don't have a safeArea)"),
+            onPressed: () => showScrollableModalWebView(
+                context: context,
+                header: Container(
+                  height: 30,
+                  decoration: const BoxDecoration(
+                    borderRadius:
+                        BorderRadius.vertical(top: Radius.circular(10.0)),
+                    color: Colors.blue,
+                  ),
+                  child: const Center(
+                    child: Text('ScrollableModalBottomSheet'),
+                  ),
                 ),
-              ),
-              backgroundColor: Colors.blue,
-              controller: controller,
-              initialChildSize: 0.96,
-              url: 'https://flutter.dev/',
-              shape: const RoundedRectangleBorder(
-                borderRadius: BorderRadius.vertical(top: Radius.circular(10.0)),
-              )),
-        ),
+                controller: controller,
+                initialChildSize: 0.8,
+                url: 'https://flutter.dev/',
+                shape: const RoundedRectangleBorder(
+                  borderRadius:
+                      BorderRadius.vertical(top: Radius.circular(10.0)),
+                )),
+          ),
+          Expanded(child: Container()),
+          TextButton(
+            child: const Text(
+                "display showScrollableModalWebView (has a safeArea but don't have a header)"),
+            onPressed: () => showScrollableModalWebView(
+                context: context,
+                backgroundColor: Colors.blue,
+                controller: controller,
+                initialChildSize: 0.96,
+                url: 'https://flutter.dev/',
+                shape: const RoundedRectangleBorder(
+                  borderRadius:
+                      BorderRadius.vertical(top: Radius.circular(10.0)),
+                )),
+          ),
+          Expanded(child: Container()),
+          TextButton(
+            child: const Text(
+                "display showScrollableModalWebView (don't have a header and safeArea)"),
+            onPressed: () => showScrollableModalWebView(
+                context: context,
+                scrollable: true,
+                controller: controller,
+                initialChildSize: 0.8,
+                url: 'https://flutter.dev/',
+                shape: const RoundedRectangleBorder(
+                  borderRadius:
+                      BorderRadius.vertical(top: Radius.circular(10.0)),
+                )),
+          ),
+          Expanded(child: Container()),
+        ],
       ),
     );
   }
