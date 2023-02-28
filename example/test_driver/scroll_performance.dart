c442e9535fc5828cf57d9bb88693b7771e9894f2
yoskoshi <8423koshi@ezweb.ne.jp>
2023-02-28 14:01:18 +0900

    add scroll performance test

---
+++ example/test_driver/scroll_performance.dart
@@ -0,0 +1,8 @@
+import 'package:example/main.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_driver/driver_extension.dart';
+
+void main() async {
+  enableFlutterDriverExtension();
+  runApp(const MyApp());
+}
