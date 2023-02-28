49fafb25d9de89ec1605e7438eb5f43f6d87a7f8
yoskoshi <8423koshi@ezweb.ne.jp>
2023-02-28 14:00:48 +0900

    change header widget test

--- example/test/widget_test.dart
+++ example/test/widget_test.dart
@@ -5,26 +5,231 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:example/main.dart';
+import 'package:scrollable_modal_webview/scrollable_modal_webview.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+import 'package:webview_flutter_android/webview_flutter_android.dart';
+import 'package:webview_flutter_wkwebview/webview_flutter_wkwebview.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('Test if header exist in IOS', (WidgetTester tester) async {
+    WebViewPlatform.instance = WebKitWebViewPlatform();
+    final WebViewController webViewController = WebViewController();
+    final header = Container(
+      height: 30,
+      decoration: const BoxDecoration(
+        color: Colors.blue,
+      ),
+      child: const Center(
+        child: Text('ScrollableModalBottomSheet'),
+      ),
+    );
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(MaterialApp(
+      home: Builder(builder: (context) {
+        return TextButton(
+            onPressed: () => showScrollableModalWebView(
+                platform: Platform.isIOS,
+                context: context,
+                scrollable: true,
+                header: header,
+                backgroundColor: Colors.blue,
+                controller: webViewController,
+                initialChildSize: 0.96,
+                url: 'https://flutter.dev/',
+                shape: const RoundedRectangleBorder(
+                  borderRadius:
+                      BorderRadius.vertical(top: Radius.circular(10.0)),
+                )),
+            child: const Text(
+                "display showScrollableModalWebView (has a header and a safeArea)"));
+      }),
+    ));
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
+    expect(
+        find.text(
+            "display showScrollableModalWebView (has a header and a safeArea)"),
+        findsOneWidget);
+    await tester.tap(find.text(
+        "display showScrollableModalWebView (has a header and a safeArea)"));
     await tester.pump();
 
     // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    expect(find.byWidget(header), findsOneWidget);
+  });
+
+  testWidgets('Test if header do not exists in IOS',
+      (WidgetTester tester) async {
+    WebViewPlatform.instance = WebKitWebViewPlatform();
+    final WebViewController webViewController = WebViewController();
+    final header = Container(
+      height: 30,
+      decoration: const BoxDecoration(
+        color: Colors.blue,
+      ),
+      child: const Center(
+        child: Text('ScrollableModalBottomSheet'),
+      ),
+    );
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(MaterialApp(
+      home: Builder(builder: (context) {
+        return TextButton(
+            onPressed: () => showScrollableModalWebView(
+                platform: Platform.isIOS,
+                context: context,
+                scrollable: true,
+                backgroundColor: Colors.blue,
+                controller: webViewController,
+                initialChildSize: 0.96,
+                url: 'https://flutter.dev/',
+                shape: const RoundedRectangleBorder(
+                  borderRadius:
+                      BorderRadius.vertical(top: Radius.circular(10.0)),
+                )),
+            child: const Text(
+                "display showScrollableModalWebView (has a header and a safeArea)"));
+      }),
+    ));
+
+    expect(
+        find.text(
+            "display showScrollableModalWebView (has a header and a safeArea)"),
+        findsOneWidget);
+    await tester.tap(find.text(
+        "display showScrollableModalWebView (has a header and a safeArea)"));
+    await tester.pump();
+
+    // Verify that our counter has incremented.
+    expect(find.byWidget(header), findsNothing);
+  });
+
+  testWidgets('Test if header exist in Android', (WidgetTester tester) async {
+    WebViewPlatform.instance = AndroidWebViewPlatform();
+    final WebViewController webViewController = WebViewController();
+    final header = Container(
+      height: 30,
+      decoration: const BoxDecoration(
+        color: Colors.blue,
+      ),
+      child: const Center(
+        child: Text('ScrollableModalBottomSheet'),
+      ),
+    );
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(MaterialApp(
+      home: Builder(builder: (context) {
+        return TextButton(
+            onPressed: () => showScrollableModalWebView(
+                platform: Platform.isAndroid,
+                context: context,
+                scrollable: true,
+                header: header,
+                backgroundColor: Colors.blue,
+                controller: webViewController,
+                initialChildSize: 0.96,
+                url: 'https://flutter.dev/',
+                shape: const RoundedRectangleBorder(
+                  borderRadius:
+                      BorderRadius.vertical(top: Radius.circular(10.0)),
+                )),
+            child: const Text(
+                "display showScrollableModalWebView (has a header and a safeArea)"));
+      }),
+    ));
+
+    expect(
+        find.text(
+            "display showScrollableModalWebView (has a header and a safeArea)"),
+        findsOneWidget);
+    await tester.tap(find.text(
+        "display showScrollableModalWebView (has a header and a safeArea)"));
+    await tester.pump();
+
+    // Verify that our counter has incremented.
+    expect(find.byWidget(header), findsOneWidget);
+  });
+
+  testWidgets('Test if header do not exist in Android',
+      (WidgetTester tester) async {
+    WebViewPlatform.instance = AndroidWebViewPlatform();
+    final WebViewController webViewController = WebViewController();
+    final header = Container(
+      height: 30,
+      decoration: const BoxDecoration(
+        color: Colors.blue,
+      ),
+      child: const Center(
+        child: Text('ScrollableModalBottomSheet'),
+      ),
+    );
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(MaterialApp(
+      home: Builder(builder: (context) {
+        return TextButton(
+            onPressed: () => showScrollableModalWebView(
+                platform: Platform.isAndroid,
+                context: context,
+                scrollable: true,
+                backgroundColor: Colors.blue,
+                controller: webViewController,
+                initialChildSize: 0.96,
+                url: 'https://flutter.dev/',
+                shape: const RoundedRectangleBorder(
+                  borderRadius:
+                      BorderRadius.vertical(top: Radius.circular(10.0)),
+                )),
+            child: const Text(
+                "display showScrollableModalWebView (has a header and a safeArea)"));
+      }),
+    ));
+
+    expect(
+        find.text(
+            "display showScrollableModalWebView (has a header and a safeArea)"),
+        findsOneWidget);
+    await tester.tap(find.text(
+        "display showScrollableModalWebView (has a header and a safeArea)"));
+    await tester.pump();
+
+    // Verify that our counter has incremented.
+    expect(find.byWidget(header), findsNothing);
+  });
+
+  testWidgets("Test if webView can be displayed in iOS",
+      (WidgetTester tester) async {
+    WebViewPlatform.instance = WebKitWebViewPlatform();
+    final WebViewController controller = WebViewController()
+      ..loadRequest(Uri.parse('https://flutter.dev/'));
+
+    final webView = WebViewWidget(controller: controller);
+
+    await tester.pumpWidget(
+      MaterialApp(home: Builder(builder: (context) {
+        return webView;
+      })),
+    );
+
+    expect(find.byWidget(webView), findsOneWidget);
+  });
+
+  testWidgets("Test if webView can be displayed in Android",
+      (WidgetTester tester) async {
+    WebViewPlatform.instance = AndroidWebViewPlatform();
+    final WebViewController controller = WebViewController()
+      ..loadRequest(Uri.parse('https://flutter.dev/'));
+
+    final webView = WebViewWidget(controller: controller);
+
+    await tester.pumpWidget(
+      MaterialApp(home: Builder(builder: (context) {
+        return webView;
+      })),
+    );
+
+    expect(find.byWidget(webView), findsOneWidget);
   });
 }
